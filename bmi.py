0462fe09f20f09773dd932b659d0e9c1a3449960
yj <yijun.24@ichat.sp.edu.sg>
2025-05-24 20:48:47 +0800

    added code to bmi file

--- bmi.py
+++ bmi.py
@@ -1,4 +1,19 @@
 def calculate_bmi(height, weight):
-    print ("Height = " + height)
-    print ("Weight = " + weight)
-calculate_bmi(weight="57", height="1.73")
+    print ("Height = " + str(height))
+    print ("Weight = " + str(weight))
+    bmi = weight / (height * height)
+    print ("BMI = " + str(bmi))
+    return bmi
+
+bmi = calculate_bmi(weight=57, height=1.73) 
+
+if bmi < 18.5:
+    print ("under weight")
+elif bmi>=18.5 and bmi<=25.0:
+    print ("nomral weight")
+else:
+    print ("over weight")
+
+
+    
+
