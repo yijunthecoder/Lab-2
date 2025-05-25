16db8e83a5daadd178eef6124b505011b2eac113
yj <yijun.24@ichat.sp.edu.sg>
2025-05-25 21:35:24 +0800

    added return values

--- bmi.py
+++ bmi.py
@@ -1,17 +1,17 @@
 def calculate_bmi(height, weight):
     print ("Height = " + str(height))
     print ("Weight = " + str(weight))
+    
     bmi = weight / (height * height)
     print ("BMI = " + str(bmi))
-    return bmi
-
-bmi = calculate_bmi(weight=57, height=1.73) 
-
-if bmi < 18.5:
-    print ("under weight")
-elif bmi>=18.5 and bmi<=25.0:
-    print ("nomral weight")
-else:
-    print ("over weight")
-
-
+    bmi = calculate_bmi(weight=57, height=1.73) 
+    
+    if (bmi < 18.5):
+        print ("under weight")
+        return -1
+    elif (bmi>18.5 and bmi<=25.0):
+        print ("nomral weight")
+        return 0
+    elif(bmi>25):
+        print ("over weight")
+        return 1
